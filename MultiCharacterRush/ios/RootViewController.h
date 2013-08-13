980f9bf23438a8eb38d82bb1cb5ebf811c47867f
jinlinsky <uki@qq.com>
2013-08-13 12:04:15 +0800

    Initial Commit

---
+++ MultiCharacterRush/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  MultiCharacterRushAppController.h
+//  MultiCharacterRush
+//
+//  Created by jinlin on 13-8-13.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
